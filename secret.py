b5f144029b36aed21a27eb9337a5c307c5f32511
Vivek V Pai <72027099+vivekvpai@users.noreply.github.com>
2023-12-31 18:28:11 +0530

    Add files via upload

---
+++ secret.py
@@ -0,0 +1,7 @@
+
+# The sender's email :
+from_email = 'vdev54321@gmail.com'  # 'example@gmail.com' - (must be a gmail account)
+from_email_password = ''  # 'password'
+
+# The reciever's email :
+to_email = 'vdev02468@gmail.com'  # 'example@example.com'
